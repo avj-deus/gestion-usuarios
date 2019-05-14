a103587fdda997dc8a9a13547b7bae13380e2237
pedrodlf <pedrodlafuente@gmail.com>
2019-05-14 21:18:26 +0200

    initial commit

---
+++ src/main/java/com/cice/gestionusuarios/service/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.cice.gestionusuarios.service;
+
+import com.cice.gestionusuarios.controller.dto.UsuarioDTO;
+
+public interface UsuarioService {
+
+    /**
+     * Metodo que buscara un usuario en base a los datos obtenidos del formulario de login
+     *
+     * @param usuarioDTO
+     * @return
+     */
+    Long buscarUsuarioByLogin(UsuarioDTO usuarioDTO);
+}
---
+++ src/main/java/com/cice/gestionusuarios/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cice.gestionusuarios.service.impl;
+
+import com.cice.gestionusuarios.controller.dto.UsuarioDTO;
+import com.cice.gestionusuarios.repository.UsuarioRepository;
+import com.cice.gestionusuarios.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public Long buscarUsuarioByLogin(UsuarioDTO usuarioDTO) {
+        return usuarioRepository.buscarUsuarioByLogin(usuarioDTO.getUser(), usuarioDTO.getPass());
+    }
+}
