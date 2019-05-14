2b7c35d13f40a4148376f033d7f2e63e40f90550
pedrodlf <pedrodlafuente@gmail.com>
2019-05-14 22:29:19 +0200

    added query annotation

--- src/main/java/com/cice/gestionusuarios/repository/UsuarioRepository.java
+++ src/main/java/com/cice/gestionusuarios/repository/UsuarioRepository.java
@@ -2,10 +2,13 @@ package com.cice.gestionusuarios.repository;
 
 import com.cice.gestionusuarios.repository.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
-    Long buscarUsuarioByLogin(String user, String pass);
+    @Query(value = "SELECT id FROM usuario WHERE user = :user AND pass = :pass", nativeQuery = true)
+    Long buscarUsuarioByLogin(@Param(value = "user") String user, @Param(value = "pass")String pass);
 }
